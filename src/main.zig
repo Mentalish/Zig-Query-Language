68017cb5fc5749f576f96d80cf2cfe35fa43c420
Erik <ej.erik.jimenez@gmail.com>
2026-01-15 16:56:23 -0600

    created lexer

--- src/main.zig
+++ src/main.zig
@@ -1,27 +1,35 @@
 const std = @import("std");
-const Zig_Query_Language = @import("Zig_Query_Language");
+const zql = @import("Zig_Query_Language");
 
 pub fn main() !void {
-    // Prints to stderr, ignoring potential errors.
-    std.debug.print("All your {s} are belong to us.\n", .{"codebase"});
-    try Zig_Query_Language.bufferedPrint();
-}
+    var in_buffer: [1024]u8 = undefined;
+    var out_buffer: [1024]u8 = undefined;
 
-test "simple test" {
-    const gpa = std.testing.allocator;
-    var list: std.ArrayList(i32) = .empty;
-    defer list.deinit(gpa); // Try commenting this out and see if zig detects the memory leak!
-    try list.append(gpa, 42);
-    try std.testing.expectEqual(@as(i32, 42), list.pop());
-}
+    var stdin_reader = std.fs.File.stdin().reader(&in_buffer);
+    const stdin: *std.io.Reader = &stdin_reader.interface;
+    
+    var stdout_writer = std.fs.File.stdout().writer(&out_buffer);
+    const stdout: *std.io.Writer = &stdout_writer.interface;
+    while (true) { 
+        try stdout.print("Enter the Query: \n", .{});
+        try stdout.flush();
 
-test "fuzz example" {
-    const Context = struct {
-        fn testOne(context: @This(), input: []const u8) anyerror!void {
-            _ = context;
-            // Try passing `--fuzz` to `zig build test` and see if it manages to fail this test case!
-            try std.testing.expect(!std.mem.eql(u8, "canyoufindme", input));
+        const buffer = try stdin.takeDelimiter('\n') orelse "";
+
+        var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+        const base_allocator = gpa.allocator();
+        defer _ = gpa.deinit();
+
+        var arena_allocator = std.heap.ArenaAllocator.init(base_allocator);
+        defer arena_allocator.deinit();
+
+        const tokens: [][]const u8 = zql.lexer.cmd_lexer(buffer, arena_allocator.allocator()) catch &[_][]const u8{};
+
+        for (tokens) |token| {
+            try stdout.print("{s} ", .{token});
         }
-    };
-    try std.testing.fuzz(Context{}, Context.testOne, .{});
+        
+        try stdout.print("\n", .{});
+        try stdout.flush();
+    }
 }
