918973e85a902a040c7851f8d24b942416502d5c
Erik <ej.erik.jimenez@gmail.com>
2026-01-15 19:28:39 -0600

    push logic for SELECT and FROM

--- src/parser/parse-tree.zig
+++ src/parser/parse-tree.zig
@@ -1,22 +1,36 @@
 const std = @import("std");
 
-const Types = enum {
-    MATH,
-    SCHEMA,
-
-};
-
 const ParseTreeNode = struct {
     operator: []const u8,
-    type: 
-    children: *ParseTreeNode,
+    parent: ?*ParseTreeNode = null,
+    children: ?*[]ParseTreeNode = null,
 };
 
-
 pub fn createParseTree(buffer: [][]const u8, allocator: std.mem.Allocator) !*ParseTreeNode {
-    for (buffer) |token| {
-        //push token
-        //check what the token was
-        //act based on rules
+    var node: *ParseTreeNode = undefined;
+    var token_stack = std.BoundedArray([]const u8, buffer.len);
+
+    var i = 0;
+    query: while (i < buffer.len) {
+        if (std.ascii.eqlIgnoreCase(buffer[i], "SELECT")) {
+            token_stack.append(buffer[i]);
+            i += 1;
+
+            while (std.ascii.isAlphabetic(buffer[i])) {
+                token_stack.append(buffer[i]);
+                i += 1;
+                break: query;
+            }
+        } else if (std.ascii.eqlIgnoreCase(buffer[i], "FROM")) {
+            token_stack.append(buffer[i]);
+            i += 1;
+            break: query;
+        } else if (std.ascii.eqlIgnoreCase(buffer[i], "WHERE")) {
+            //grab operators
+        } else {
+            unreachable; //bad operator
+        }
     }
+
+    return node;
 }
