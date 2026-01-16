53569f41bdf7717ca5e1a5a62a6883053578f7d7
Erik <ej.erik.jimenez@gmail.com>
2026-01-16 10:07:58 -0600

    state tree structs

--- src/parser/parse-tree.zig
+++ src/parser/parse-tree.zig
@@ -6,30 +6,30 @@ const ParseTreeNode = struct {
     children: ?*[]ParseTreeNode = null,
 };
 
-const StackItem = union(enum){
+const StackItem = union(enum) {
     tree: ?*ParseTreeNode,
     string: ?[]const u8,
 };
 
-pub fn createParseTree(buffer: [][]const u8, allocator: std.mem.Allocator) !*ParseTreeNode {
+pub fn createParseTree(buffer: [][]const u8, state_tree: []State, allocator: std.mem.Allocator) !*ParseTreeNode {
     var node: *ParseTreeNode = undefined;
     var token_stack = std.BoundedArray(StackItem, buffer.len);
 
     var i = 0;
     query: while (i < buffer.len) {
         if (std.ascii.eqlIgnoreCase(buffer[i], "SELECT")) {
-            token_stack.append(.{.string = .{buffer[i]}});
+            token_stack.append(.{ .string = .{buffer[i]} });
             i += 1;
 
             while (std.ascii.isAlphabetic(buffer[i])) {
-                token_stack.append(.{.string = .{buffer[i]}});
+                token_stack.append(.{ .string = .{buffer[i]} });
                 i += 1;
-                break: query;
+                break :query;
             }
         } else if (std.ascii.eqlIgnoreCase(buffer[i], "FROM")) {
-            token_stack.append(.{.string = .{buffer[i]}});
+            token_stack.append(.{ .string = .{buffer[i]} });
             i += 1;
-            break: query;
+            break :query;
         } else if (std.ascii.eqlIgnoreCase(buffer[i], "WHERE")) {
             //grab operators
         } else {
@@ -38,4 +38,24 @@ pub fn createParseTree(buffer: [][]const u8, allocator: std.mem.Allocator) !*Par
     }
 
     return node;
+}
+
+const StateTree = struct { state: []State, num_states: usize };
+
+const State = struct { sub_states: std.StringHashMap([]const u8) };
+
+pub fn initStateTree(allocator: std.mem.Allocator) StateTree {
+    //init state tree
+    var state_tree: StateTree = allocator.alloc(StateTree, 1);
+    state_tree.num_states = 4;
+
+    //init substates
+    for (state_tree.state) |state| {
+        state.sub_states = std.StringHashMap([]const u8).init(allocator);
+    }
+
+    //populate state tree
+    try state_tree.state[0].stubstates.put("SELECT", "s3");
+
+    return state_tree;
 }
