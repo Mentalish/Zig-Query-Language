1ef17feea0fcc062cc9436675a133b3c1ed162bc
Erik <ej.erik.jimenez@gmail.com>
2026-01-15 20:20:57 -0600

    added multiple types to the stack

--- src/parser/parse-tree.zig
+++ src/parser/parse-tree.zig
@@ -6,23 +6,28 @@ const ParseTreeNode = struct {
     children: ?*[]ParseTreeNode = null,
 };
 
+const StackItem = union(enum){
+    tree: ?*ParseTreeNode,
+    string: ?[]const u8,
+};
+
 pub fn createParseTree(buffer: [][]const u8, allocator: std.mem.Allocator) !*ParseTreeNode {
     var node: *ParseTreeNode = undefined;
-    var token_stack = std.BoundedArray([]const u8, buffer.len);
+    var token_stack = std.BoundedArray(StackItem, buffer.len);
 
     var i = 0;
     query: while (i < buffer.len) {
         if (std.ascii.eqlIgnoreCase(buffer[i], "SELECT")) {
-            token_stack.append(buffer[i]);
+            token_stack.append(.{.string = .{buffer[i]}});
             i += 1;
 
             while (std.ascii.isAlphabetic(buffer[i])) {
-                token_stack.append(buffer[i]);
+                token_stack.append(.{.string = .{buffer[i]}});
                 i += 1;
                 break: query;
             }
         } else if (std.ascii.eqlIgnoreCase(buffer[i], "FROM")) {
-            token_stack.append(buffer[i]);
+            token_stack.append(.{.string = .{buffer[i]}});
             i += 1;
             break: query;
         } else if (std.ascii.eqlIgnoreCase(buffer[i], "WHERE")) {
