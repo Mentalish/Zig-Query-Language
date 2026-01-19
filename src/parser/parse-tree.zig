5993a341093ab604aa144b4c386aba93eb1ea32c
Erik <ej.erik.jimenez@gmail.com>
2026-01-19 10:20:29 -0600

    reworked action selector

--- src/parser/parse-tree.zig
+++ src/parser/parse-tree.zig
@@ -3,6 +3,7 @@ const typ = @import("lexer.zig");
 
 pub const ParseTreeNode = struct {
     operator: []const u8,
+    type_code: typ.TokenType,
     parent: ?*ParseTreeNode = null,
     children: ?*[]ParseTreeNode = null,
 };
@@ -29,30 +30,57 @@ const StateDictionary = struct { state: []State, num_states: usize };
 
 const State = struct { sub_states: std.StringHashMap([]const u8) };
 
-pub fn create_parse_tree(buffer: [][]const u8, state_tree: []State, allocator: std.mem.Allocator) !*ParseTreeNode {
-    var node_stack: std.ArrayList(StackItem) = {};
-    node_stack.append(try create_node(buffer[0], allocator));
+pub fn create_parse_tree(tokens: []typ.Token, allocator: std.mem.Allocator) !*ParseTreeNode {
+    var node_stack: std.ArrayList(ParseTreeNode) = {};
+    node_stack.append(try create_node(tokens[0], allocator));
     var i: usize = 1;
+    var j: usize = 0;
     var current_state: usize = 0;
-    while (i < buffer.len) {
-        var opcode: ActionCode = try state_tree.state[current_state].get(peak(node_stack)) orelse break;
-        //do actions based on code
-        //push next thing to the stack
-        node_stack.append(create_node(buffer[i], allocator));
-        i += 1;
+    while (i < tokens.len) {
+        const action_code: typ.TokenType = try get_action_code(node_stack);
+        const action: SubState = state_dictionary[current_state][action_code];
+
+        const opcode = action.action;
+        const action_count = action.action_count;
+        current_state = action.next_state;
+        switch (opcode) {
+            .push => {
+                while (i < action_count) : (j += 1) {
+                    node_stack.append(create_node(tokens[i], allocator));
+                    i += 1;
+                }
+                j = 0;
+            },
+            .reduce => {
+                while (i < action_count) : (j += 1) {}
+                j = 0;
+            },
+            .eof => {},
+            .@"error" => return error.Invalid_Token,
+            else => unreachable,
+        }
     }
 }
 
-fn create_node(operator: []const u8, allocator: std.mem.Allocator) !*ParseTreeNode {
+fn create_node(tokens: typ.Token, allocator: std.mem.Allocator) !*ParseTreeNode {
     var node: *ParseTreeNode = try allocator.create(ParseTreeNode);
-    node.operator = operator;
+    node.operator = tokens.token;
+    node.type_code = tokens.type;
     return node;
 }
 
-fn peak(stack: std.ArrayList(StackItem)) ?[]const u8 {
+fn get_action_code(stack: std.ArrayList(ParseTreeNode)) ?ActionCode {
     if (stack.len != 0) {
-        return stack[stack.len - 1].operator;
+        return stack[stack.len - 1].type_code;
     }
+}
+
+pub fn free_parse_tree(root: *ParseTreeNode, allocator: std.mem.Allocator) void {
+    for (root.children) |child| {
+        free_parse_tree(child, allocator);
+    }
+
+    allocator.destroy(root);
 }
 
 const state_dictionary = [_]?[]const SubState{
@@ -98,11 +126,3 @@ const state_dictionary = [_]?[]const SubState{
         break :state_4 &final;
     },
 };
-
-pub fn free_parse_tree(root: *ParseTreeNode, allocator: std.mem.Allocator) void {
-    for (root.children) |child| {
-        free_parse_tree(child, allocator);
-    }
-
-    allocator.destroy(root);
-}
