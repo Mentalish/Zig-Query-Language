e26df4a41d06e4512eae904da602ad40932e88c0
Erik <ej.erik.jimenez@gmail.com>
2026-01-24 18:03:15 -0600

    schema structures

--- src/root.zig
+++ src/root.zig
@@ -1,2 +1,4 @@
 //! By convention, root.zig is the root source file when making a library.
 pub const lexer = @import("parser/lexer.zig");
+pub const schema_store = @import("schema/schema_structure.zig");
+pub const schema_fun = @import("schema/schema_manipulate.zig");
---
+++ src/schema/schema_structure.zig
@@ -0,0 +1,21 @@
+const Schema = struct {
+    name: []const u8,
+    tables: *[]TableSchema,
+};
+
+const TableSchema = struct {
+    name: []const u8,
+    items: Data,
+    foreign_keys: ?[][]const u8 = null,
+};
+
+const Data = struct {
+    name: []const u8,
+    type: DataType,
+};
+
+const DataType = enum {
+    INT,
+    FLOAT,
+    STRING,
+};
