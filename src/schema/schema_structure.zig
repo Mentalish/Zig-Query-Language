85a8ecb113df089c4001f93860d7046180f058d0
Erik <44383316+Mentalish@users.noreply.github.com>
2026-01-27 22:28:22 -0600

    Refactor/dev 8 schema store refactor (#7)
    * refactored schema structure
    
    * schema create functions refactored

--- src/schema/schema_structure.zig
+++ src/schema/schema_structure.zig
@@ -2,17 +2,17 @@ const std = @import("std");
 
 const Database = struct {
     name: []const u8,
-    schemas: std.StringHashMap(*Schema),
+    schema: *Schema,
 };
 
 const Schema = struct {
     name: []const u8,
-    tables: *[]TableSchema,
+    tables: std.StringHashMap(*TableSchema),
 };
 
 const TableSchema = struct {
     name: []const u8,
-    items: *[]Data,
+    items: std.StringHashMap(*Data),
     foreign_keys: ?[][]const u8 = null,
 };
 
